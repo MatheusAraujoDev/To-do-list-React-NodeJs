80cb84176df6a5e66a4c550f5c768649c9b079a5
Matheus <matheusaraujo.dev@gmail.com>
2021-11-02 22:40:29 -0300

    CRUD do frontend pronto

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,69 @@
 import './App.css';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+
 function App() {
+
   const [input, setInput] = useState('');
   const [task, setTask] = useState([]);
+  // const [taskFullTable, setTaskFullTable] = useState([]);
+  const [taskDone, setTaskDone] = useState([]);
 
-  useEffect(() => {
-    getTasksFromDb();
-  }, []);
+  useEffect(()=>{
+    getTasksFromDb()
+  },[]);
 
-  const getTasksFromDb = () => {
+  function editInput(taskNameEdit) {
+    return prompt("Edite sua tarefa:", taskNameEdit)
+  }
+
+  // function allTasks() {
+  //   setTask(taskFullTable);
+  // }
+
+  function onlyConcludedTasks() {
+      let concludedTasks = []
+      task.forEach((item, index) => {
+      if(taskDone.includes(item._id)) {
+        concludedTasks.push({
+          _id: item._id,
+          task: item.task
+        })
+      }
+    })
+
+    setTask([...concludedTasks]);
+  }
+
+//   function onlyNotConcludedTasks() {
+//     let notConcludedTasks = []
+//     task.forEach((item, index) => {
+//     if(!taskDone.includes(item._id)) {
+//       notConcludedTasks.push({
+//         _id: item._id,
+//         task: item.task
+//       })
+//     }
+//   })
+//   setTask(notConcludedTasks);
+// }
+
+  const getTasksFromDb = () => {    
     axios.get('http://localhost:3001/list')
     .then(function (response) {
       setTask(response.data);
+      // setTaskFullTable(response.data)
     })
+  }
+
+  const updateTask = (_id,task) => {
+    axios.put('http://localhost:3001/update', {
+      _id: _id,
+       task: task,
+    }).then(function (response) {
+      getTasksFromDb();
+      console.log(response)
+    }) 
   }
 
   const createTask = () => {
@@ -39,7 +89,24 @@ function App() {
 
   }
 
-  return (
+  const markTask = (id) => {
+    const element = document.getElementById(`${id}`);
+    const isChecked = document.getElementById(`checkbox${id}`).checked;
+    
+    if(isChecked) {
+      setTaskDone([...taskDone, id]) // adiciona ao state o id da tarefa concluída
+    }else {
+      const findTaskIndex = taskDone.findIndex(item => item === id);
+      let array = taskDone; 
+      array.splice(findTaskIndex, 1);
+      setTaskDone([...array]);
+    }
+
+    return element.classList.toggle('markItem');
+  }
+
+
+  return (    
     <main>
       <h1>To Do List</h1>    
 
@@ -48,37 +115,56 @@ function App() {
           <tr> 
             <th>Status</th>
             <th>Nome da Tarefa</th>
-            <th>Deletar Tarefa</th>
+            <th>Opcões</th>
           </tr>
         </thead>
+     
         <tbody>
-          {task.map((item,index) => {
+
+          {task.map((item) => {
             return (
-             <tr key = {index}>
-              <td><input type="checkbox" /></td>
-              <td><h2>{item.task}</h2></td>
-              <td><button onClick = {() => {
-                deleteTask(item._id)
-              }}>X</button></td>
+             <tr key={item._id} >
+              <td>
+                <input
+                  type="checkbox"
+                  id={`checkbox${item._id}`}
+                  onClick={ () => { markTask(item._id) }} 
+                />
+              </td>
+              <td>
+                <h2 id={item._id} >{item.task}</h2></td>
+              <td>
+                <button onClick={() => {
+                  const task = editInput(item.task);
+                  updateTask(item._id, task)
+                }}> 
+                  Editar
+                </button>
+                <button onClick={() => {
+                  deleteTask(item._id)
+                }}>X</button>
+              </td>
             </tr> 
             )
           })}
         </tbody>    
       </table>
-
+    
       <div className="filter-buttons">
-        <button onClick={getTasksFromDb}>Todas as tarefas</button>
-        <button>Apenas concluídas</button>
-        <button>Apenas pendentes</button>
+        <button onClick={getTasksFromDb}>Tarefas pendentes</button>
+        <button onClick={onlyConcludedTasks}>Apenas concluídas</button>
+        {/* <button onClick={() => {
+          onlyNotConcludedTasks();
+        }}>Apenas pendentes</button> */}
       </div>
 
       <div className="addTask">
-            <input id="addInput" onChange = {(event) => {
+            <input id="addInput" onChange={(event) => {
                 setInput(event.target.value);
               }} />
-            <button id="AddButon" onClick = {createTask}>Adicionar Tarefa</button>
+            <button id="AddButon" onClick={createTask}>Adicionar Tarefa</button>
       </div>    
-    </main>
+    </main>    
   );
 }
 

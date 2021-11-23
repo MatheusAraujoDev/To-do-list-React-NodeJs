725d678c442a0d1268f0edf3566236ae545b60b9
Matheus <matheusaraujo.dev@gmail.com>
2021-11-23 10:40:24 -0300

    Instala Mongo Memory-Server

---
+++ backend/tests/CreateTask.test.js
@@ -0,0 +1,7 @@
+const { expect } = require('chai');
+
+describe('Testa a Rota de Criar uma Tarefa', () => {
+  describe('Insere uma nova tarefa ao Banco de Dados - Model', () => {
+    it('', async () => {});
+  });
+});
